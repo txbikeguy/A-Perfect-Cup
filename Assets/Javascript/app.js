0227a2c7d4f9eab98b8685492bfdf455bb5f3653
Ben_Cross <txbikeguy@gmail.com>
2018-01-13 13:37:19 -0600

    commented out maps

--- Assets/Javascript/app.js
+++ Assets/Javascript/app.js
@@ -1,66 +1,66 @@
-var pos;
-var map, infoWindow;
-      function initMap() {
-        map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: 44.9778, lng: -93.2650},
-          zoom: 12
-        });
-        infoWindow = new google.maps.InfoWindow();
-        marker = new google.maps.Marker({
-          map: map
-        })
-        var service = new google.maps.places.PlacesService(map);
-        service.nearbySearch({
-          location: pos,
-          radius: 500,
-          type: ['coffee shop']
-        }, callback);
-      };
-      function callback(results, status) {
-        if (status === google.maps.places.PlacesServiceStatus.OK) {
-          for (var i = 0; i < results.length; i++) {
-            createMarker(results[i]);
-          }
-        }
-      };
-      function createMarker(place) {
-        var placeLoc = place.geometry.location;
-        var marker = new google.maps.Marker({
-          map: map,
-          position: place.geometry.location
-        });
+// var pos;
+// var map, infoWindow;
+//       function initMap() {
+//         map = new google.maps.Map(document.getElementById('map'), {
+//           center: {lat: 44.9778, lng: -93.2650},
+//           zoom: 12
+//         });
+//         infoWindow = new google.maps.InfoWindow();
+//         marker = new google.maps.Marker({
+//           map: map
+//         })
+//         var service = new google.maps.places.PlacesService(map);
+//         service.nearbySearch({
+//           location: pos,
+//           radius: 500,
+//           type: ['coffee shop']
+//         }, callback);
+//       };
+//       function callback(results, status) {
+//         if (status === google.maps.places.PlacesServiceStatus.OK) {
+//           for (var i = 0; i < results.length; i++) {
+//             createMarker(results[i]);
+//           }
+//         }
+//       };
+//       function createMarker(place) {
+//         var placeLoc = place.geometry.location;
+//         var marker = new google.maps.Marker({
+//           map: map,
+//           position: place.geometry.location
+//         });
 
-        google.maps.event.addListener(marker, 'click', function() {
-          infowindow.setContent(place.name);
-          infowindow.open(map, this);
-        });
-      };
+//         google.maps.event.addListener(marker, 'click', function() {
+//           infowindow.setContent(place.name);
+//           infowindow.open(map, this);
+//         });
+//       };
 
-        // Try HTML5 geolocation.
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
-            };
+//         // Try HTML5 geolocation.
+//         if (navigator.geolocation) {
+//           navigator.geolocation.getCurrentPosition(function(position) {
+//             var pos = {
+//               lat: position.coords.latitude,
+//               lng: position.coords.longitude
+//             };
 
-            marker.setPosition(pos);
-            infoWindow.open(map);
-            map.setCenter(pos);
-          }, function() {
-            handleLocationError(true, infoWindow, map.getCenter());
-          });
-        } else {
-          // Browser doesn't support Geolocation
-          handleLocationError(false, infoWindow, map.getCenter());
-        };
+//             marker.setPosition(pos);
+//             infoWindow.open(map);
+//             map.setCenter(pos);
+//           }, function() {
+//             handleLocationError(true, infoWindow, map.getCenter());
+//           });
+//         } else {
+//           // Browser doesn't support Geolocation
+//           handleLocationError(false, infoWindow, map.getCenter());
+//         };
 
-      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(browserHasGeolocation ?
-                              'Error: The Geolocation service failed.' :
-                              'Error: Your browser doesn\'t support geolocation.');
-        infoWindow.open(map);
-      }
+//       function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+//         infoWindow.setPosition(pos);
+//         infoWindow.setContent(browserHasGeolocation ?
+//                               'Error: The Geolocation service failed.' :
+//                               'Error: Your browser doesn\'t support geolocation.');
+//         infoWindow.open(map);
+//       }
 
 
