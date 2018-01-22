e08ac771f54db2ceeb0174d5b2ead4117dce2ff3
Ben_Cross <txbikeguy@gmail.com>
2018-01-22 17:45:46 -0600

    added more to weather output

--- Assets/Javascript/weather.js
+++ Assets/Javascript/weather.js
@@ -31,13 +31,9 @@ function getWeather(latitude,longitude) {
 			currWeather['currTemp']			= Math.round(data.main.temp);				// current temperature
 			currWeather['highTemp']			= Math.round(data.main.temp_max);			// today's high temp
 			currWeather['lowTemp']			= Math.round(data.main.temp_min);			// today's low temp
-			currWeather['humidity']			= Math.round(data.main.humidity);			// humidity (in percent)
-			currWeather['pressure']			= data.main.pressure * 0.02961339710085;	// barometric pressure (converting hPa to inches)
-			currWeather['pressure']			= currWeather['pressure'].toFixed(2);		// barometric pressure (rounded to 2 decimals)
 			
 			currWeather['description']		= data.weather[0].description;				// short text description (ie. rain, sunny, etc.)
 			currWeather['icon']				= "http://openweathermap.org/img/w/"+data.weather[0].icon+".png";	// 50x50 pixel png icon
-			currWeather['cloudiness']		= data.clouds.all;							// cloud cover (in percent)
 			currWeather['windSpeed']		= Math.round(data.wind.speed);				// wind speed
 			
 			currWeather['windDegree']		= data.wind.deg;							// wind direction (in degrees)
@@ -45,24 +41,28 @@ function getWeather(latitude,longitude) {
 			
 			// array of direction (compass) names
 			var windNames					= new Array("North","North Northeast","Northeast","East Northeast","East","East Southeast", "Southeast", "South Southeast","South","South Southwest","Southwest","West Southwest","West","West Northwest","Northwest","North Northwest");
-			// array of abbreviated (compass) names
-			var windShortNames				= new Array("N","NNE","NE","ENE","E","ESE", "SE", "SSE","S","SSW","SW","WSW","W","WNW","NW","NNW");
+
 			currWeather['windDirection']	= windNames[currWeather['windCompass']];	// convert degrees and find wind direction name
 			
 			
-			var response 		= "Current Weather: "+currWeather['currTemp']+"\xB0 and "+currWeather['description'];
+			var response 		= "It is currently "+currWeather['currTemp']+"\xB0 and "+currWeather['description'];
 			
 			if(currWeather['windSpeed']>0) {											// if there's wind, add a wind description to the response
-				response		= response+" with winds out of the "+windNames[currWeather['windCompass']]+" at "+currWeather['windSpeed'];
+				response		= response + " with winds" + " at " +currWeather['windSpeed'];
 				if(currWeather['windSpeed']==1) {
 					response		+= " mile per hour";
 				} else {
 					response		+= " miles per hour";
 				}
 			}
+			var response2 = "<br><br>The high for today is " + currWeather['highTemp'] + "\xB0 and the low tonight will be " + currWeather['lowTemp'] + "\xB0"; 
+			var response3 = "<img src='" + currWeather['icon'] + "'>";
+
 			
 			console.log(data);												// log weather data for reference (json format) 
 			$("#weather").html(response);									// write current weather to textarea
+			$("#weather").append(response2);
+			$("#icon").append(response3);
 			$("#location-lat-long").html("<strong>" + data.name + "</strong><br><br>Latitude: " + data.coord.lat + " / Longitude: " + data.coord.lon)
 		});
 	} else {
